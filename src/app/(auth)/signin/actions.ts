8d2769fb6169e53276a3f51d8e0a3ada6601f263
X_V.neer <xv.neer@gmail.com>
2024-07-26 21:23:30 +0300

    feat: create signin server action

---
+++ src/app/(auth)/signin/actions.ts
@@ -0,0 +1,60 @@
+"user server"
+
+import { isRedirectError } from "next/dist/client/components/redirect"
+import { cookies } from "next/headers"
+import { redirect } from "next/navigation"
+import { signInSchema, SignInValues } from "@/validation/auth"
+import { verify } from "@node-rs/argon2"
+
+import { lucia } from "@/lib/lucia"
+import prisma from "@/lib/prisma"
+
+export async function logIn(credentials: SignInValues): Promise<{ error: string }> {
+  try {
+    const { data, error, success } = signInSchema.safeParse(credentials)
+    if (!success)
+      return {
+        error: error.message || "Invalid credentials",
+      }
+
+    const { username, password } = data
+    const user = await prisma.user.findFirst({
+      where: {
+        username: {
+          equals: username,
+          mode: "insensitive",
+        },
+      },
+    })
+
+    if (!user || !user.passwordHash) {
+      return {
+        error: "Incorrect username or password",
+      }
+    }
+
+    const validPassword = await verify(user.passwordHash, password, {
+      memoryCost: 19456,
+      timeCost: 2,
+      outputLen: 32,
+      parallelism: 1,
+    })
+
+    if (validPassword) {
+      return {
+        error: "Incorrect username or password",
+      }
+    }
+
+    const session = await lucia.createSession(user.id, {})
+    const sessionCookie = lucia.createSessionCookie(session.id)
+    cookies().set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes)
+
+    return redirect("/")
+  } catch (error) {
+    if (isRedirectError(error)) throw error
+    return {
+      error: "something went wrong!, please try again",
+    }
+  }
+}
