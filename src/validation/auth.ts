1ef1d7d3bc6d5e52a35715ff7a2a4ea569558dca
X_V.neer <xv.neer@gmail.com>
2024-07-26 21:01:40 +0300

    feat:ceate intial singup server action

--- src/validation/auth.ts
+++ src/validation/auth.ts
@@ -1,10 +1,11 @@
 import { z } from "zod"
 
 export const singUpSchema = z.object({
-  email: z.string().email("Invalid email address"),
+  email: z.string().toLowerCase().email("Invalid email address"),
   username: z
     .string({ required_error: "username is required" })
     .trim()
+    .toLowerCase()
     .regex(/^[a-zA-Z0-9_-]+$/, "username must only contain letters , numbers - and _")
     .min(3, "username must be at least 3 characters")
     .max(20, "username must be less than 20 characters"),
@@ -17,7 +18,11 @@ export const singUpSchema = z.object({
 export type SingUpValues = z.infer<typeof singUpSchema>
 
 export const signInSchema = z.object({
-  username: z.string({ required_error: "username is required" }).trim().min(1, "username is required"),
+  username: z
+    .string({ required_error: "username is required" })
+    .trim()
+    .toLowerCase()
+    .min(1, "username is required"),
   password: z
     .string({ required_error: "password is required" })
     .min(8, "password must be at least 8 characters")
