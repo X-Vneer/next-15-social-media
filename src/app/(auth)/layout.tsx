f21a6eab92174f160a7296bf0585405e86bdd774
X_V.neer <xv.neer@gmail.com>
2024-07-27 23:48:27 +0300

    feat: redirect user to / if already loged in

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,4 +1,11 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
+import { redirect } from "next/navigation"
+
+import { validateRequest } from "@/lib/lucia"
+
+export default async function Layout({ children }: { children: React.ReactNode }) {
+  const { user } = await validateRequest()
+  if (user) redirect("/")
+
   return (
     <main className="flex h-screen items-center justify-center p-5">
       <div className="flex h-full max-h-[40rem] w-full max-w-[64rem] overflow-hidden rounded-lg bg-card shadow-xl">
