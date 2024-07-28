8d7383977b8926452dce17e93a50f862bc79ff3f
X_V.neer <xv.neer@gmail.com>
2024-07-28 20:24:12 +0300

    feat: ceate side bar

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -3,6 +3,7 @@ import { SessionProvider } from "@/providers/session-provider"
 
 import { validateRequest } from "@/lib/lucia"
 import Navbar from "@/components/common/header"
+import SideBar from "@/components/common/side-bar"
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
   const result = await validateRequest()
@@ -10,7 +11,11 @@ export default async function Layout({ children }: { children: React.ReactNode }
   return (
     <SessionProvider value={result}>
       <Navbar />
-      {children}
+      <div className="mx-auto flex w-full max-w-7xl grow gap-5 p-5">
+        <SideBar className="sticky top-[5.25rem] hidden h-fit flex-none space-y-3 rounded-2xl bg-card px-3 py-5 shadow-sm sm:block lg:px-5 xl:w-80" />
+        {children}
+      </div>
+      <SideBar className="sticky bottom-0 flex w-full justify-center gap-5 border-t bg-card p-3 sm:hidden" />
     </SessionProvider>
   )
 }
