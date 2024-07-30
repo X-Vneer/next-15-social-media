561c8b3916129690f8123ee4b742e8a92eebca0f
X_V.neer <xv.neer@gmail.com>
2024-07-30 17:50:59 +0300

    feat: intilize react query provider

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,4 +1,5 @@
 import { redirect } from "next/navigation"
+import ReactQueryProvider from "@/providers/react-query-provider"
 import { SessionProvider } from "@/providers/session-provider"
 
 import { validateRequest } from "@/lib/lucia"
@@ -9,13 +10,15 @@ export default async function Layout({ children }: { children: React.ReactNode }
   const result = await validateRequest()
   if (!result.user) redirect("/login")
   return (
-    <SessionProvider value={result}>
-      <Navbar />
-      <div className="mx-auto flex w-full max-w-7xl grow gap-5 p-5">
-        <SideBar className="sticky top-[5.25rem] hidden h-fit flex-none space-y-3 rounded-2xl bg-card px-3 py-5 shadow-sm sm:block lg:px-5 xl:w-80" />
-        {children}
-      </div>
-      <SideBar className="sticky bottom-0 flex w-full justify-center gap-5 border-t bg-card p-3 sm:hidden" />
-    </SessionProvider>
+    <ReactQueryProvider>
+      <SessionProvider value={result}>
+        <Navbar />
+        <div className="mx-auto flex w-full max-w-7xl grow gap-5 p-5">
+          <SideBar className="sticky top-[5.25rem] hidden h-fit flex-none space-y-3 rounded-2xl bg-card px-3 py-5 shadow-sm sm:block lg:px-5 xl:w-80" />
+          {children}
+        </div>
+        <SideBar className="sticky bottom-0 flex w-full justify-center gap-5 border-t bg-card p-3 sm:hidden" />
+      </SessionProvider>
+    </ReactQueryProvider>
   )
 }
--- src/components/posts/editor/editor.tsx
+++ src/components/posts/editor/editor.tsx
@@ -5,7 +5,6 @@ import Placeholder from "@tiptap/extension-placeholder"
 import { EditorContent, useEditor } from "@tiptap/react"
 import StarterKit from "@tiptap/starter-kit"
 
-import { Button } from "@/components/ui/button"
 import UserAvatar from "@/components/ui/user-avatar"
 
 import { submitPost } from "./actions"
